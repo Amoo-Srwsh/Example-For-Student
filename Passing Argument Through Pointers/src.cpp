7b4b163a7ad3510330d644c430ae81f19d4275ee
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-07-12 06:33:04 -0700

    add file

---
+++ Passing Argument Through Pointers/src.cpp
@@ -0,0 +1,40 @@
+//Amoo-Srwsh
+//Passing Argument through pointer : sort
+#include <iostream>
+using namespace std;
+
+void swap(int * , int *);
+void sort(int [] , int);
+
+main()
+{
+    const int size = 6;
+    int array[size] = {2,4,6,3,1,5};
+
+    sort(array,size);
+
+    for(int j=0; j<size; j++)   //after sort print array
+        cout << array[j] << "   ";
+
+
+}
+
+void sort(int array[] , int size)
+{
+    int i,j;
+    int *pointer;
+    pointer = array;
+
+    for(i=0; i<size; i++)
+        for(j=0; j<size-1; j++)
+            if(*(pointer+j) > *(pointer+j+1))
+                swap((pointer+j),(pointer+j+1));  //true swap index array number
+}
+
+void swap(int *pa , int *pb)
+{
+    int temp;
+    temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+}
