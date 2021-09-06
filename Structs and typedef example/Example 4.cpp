20bbc86a118409e844bc0f92afd134c5dc6f739e
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-09-06 18:56:31 +0300

    Add files via upload

---
+++ Structs and typedef example/Example 4.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<iomanip>
+using namespace std;
+
+           struct item{
+                  int id;
+                  string iname;
+                  int price;
+                  };
+
+ struct Order{
+           int day;
+           int month;
+           int year;
+           int quantity;
+           item warmss;
+           };
+
+    struct Name{
+           string last;
+           string first;
+           };
+    
+    struct Customer{
+           struct Name cool;
+           string contact;
+           struct Order hot;
+           };
+
+int main ()
+{  
+    Customer C;
+    Order O;
+    cin >> C.hot.day;
+    cout << C.hot.month;
+    system("pause");
+    return 0;
+}
