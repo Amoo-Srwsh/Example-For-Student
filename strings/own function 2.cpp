3fcee9cb9fe71f5aee198f97ef5e9488ec7aa8e4
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-10-27 17:55:20 -0400

    Add files via upload

---
+++ strings/own function 2.cpp
@@ -0,0 +1,16 @@
+/* write your own strcpy() */
+#include <iostream>
+using namespace std;
+
+void * StrCpy(char *a,const char *b)
+{
+	while ((*a++ = *b++) != '\0');
+}
+int main()
+{
+	char a[80];
+	char b[80] = "Hello This Is sroush"; 
+	
+	StrCpy(a,b);
+	cout << a << endl;
+}
