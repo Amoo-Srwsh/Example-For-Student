1013e89fa8af1df0b3edde094b23386164bb399d
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2022-03-09 17:36:53 -0500

    Add files via upload

---
+++ missing-name.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char names[4][6] = {"Alice",
+                    "Bob",
+                    "Cindy",
+                    "Dani"};
+int StrCmp(char *a)
+{
+	for (int i=0; i<4; i++) {
+		bool find=true;
+		for (int j=0; a[j]!='\0'; j++)
+			if (a[j]!=names[i][j])
+				find=false;
+		if (find==true)
+			return i+1;
+	}
+	return -1;
+}
+int main()
+{
+	int s=0;
+	for (int i=0; i<3; i++) {
+		char name[6]; cin >> name;
+		int val=StrCmp(name);
+		if (val!=-1)
+			s+=val;
+	}
+	cout << names[(10-s)-1];
+}
